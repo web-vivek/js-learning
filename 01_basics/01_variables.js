e6baa099877ca0108ccacfbd6263e663faef0a67
web-vivek <144231590+web-vivek@users.noreply.github.com>
2024-02-13 15:58:02 +0000

    variables let,const,var in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accounId = 144553;
+let accountEmail = "test@gmail.com";
+var accountPassword = "12345";
+accountCity = "mumbai"
+let accountState;
+
+// console.log();
+
+console.table([accounId, accountEmail,accountPassword, accountCity, accountState])
